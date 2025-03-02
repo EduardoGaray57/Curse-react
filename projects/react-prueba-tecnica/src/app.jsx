15895068f80980bd44d795262bc597f5d33d0aa4
EduardoGaray57 <eduardomarcelo03@gmail.com>
2025-03-02 18:12:33 -0300

    fin parte 3

--- projects/react-prueba-tecnica/src/app.jsx
+++ projects/react-prueba-tecnica/src/app.jsx
@@ -1,22 +1,45 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?
-// size=50&color=red&json=true`
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 export function App() {
-    const [ fact,  setFact] = useState('lorem ipsum cat fact whater')
+    const [ fact,  setFact] = useState()
+    const [ imagenUrl, setImagenUrl] = useState()
+    const [factError, setFactError] = useState()
 
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
-            .then(data => setFact(data.fact))
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+            })
     }, [])
+
+    useEffect(() => {
+        if (!fact) return
+        const threefirstWord = fact.split(' ', 3).join(' ')
+        console.log(threefirstWord)
+        fetch(`https://cataas.com/cat/says/${threefirstWord}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                const { url } = data
+                setImagenUrl(url)
+            })
+    }, [fact])
 
     return (
         <main>
             <h1>App de gatitos</h1>
-            <p>{fact}</p>
+            <section>
+                {fact && <p>{fact}</p>}
+                {imagenUrl &&<img src={imagenUrl} alt={`Image extracted using the 
+                    first trhee words for ${fact}`} />}
+            </section>
+            
         </main>
         
         
