8b27475ac4655c83d48daa73bb3f23ef6567238e
EduardoGaray57 <eduardomarcelo03@gmail.com>
2025-05-13 15:32:56 -0400

    filtros

--- projects/05-shopping-cart/src/components/Filters.jsx
+++ projects/05-shopping-cart/src/components/Filters.jsx
@@ -1,21 +1,42 @@
+import { useState } from 'react'
 import './Filters.css'
 
-export function Filters() {
+
+export function Filters({ onChange }) {
+    const [minPrice, setMinPrice] = useState(0)
+
+    const handelChangeMinPrice = (event) => {
+        setMinPrice(event.target.value)
+        onChange(pervState => ({
+            ...pervState,
+            minPrice: event.target.value
+        }))
+    }
+
+    const handelChangeCategory = (event) => {
+        onChange(pervState => ({
+            ...pervState,
+            category: event.target.value
+        }))
+    }
+
     return (
         <sectio className="filters">
             <div>
-                <label htmlFor="price">Price</label>
+                <label htmlFor="price">Precio a partir de:</label>
                 <input
                     type="range"
                     id="price"
                     min="0"
                     max="1000"
+                    onChange={handelChangeMinPrice}
                 />
+                <span>${minPrice}</span>
             </div>
 
             <div>
                 <label htmlFor="Category"> Categoria</label>
-                <select id="category">
+                <select id="category" onChange={handelChangeCategory}>
                     <option value="all">Todas</option>
                     <option value="beauty">Belleza</option>
                     <option value="fragrances">Fragancias</option>
