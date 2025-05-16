3913a17c790b3cae8566e34263d237eedba9d802
EduardoGaray57 <eduardomarcelo03@gmail.com>
2025-05-16 15:24:52 -0400

    Inicio de contexto

---
+++ projects/05-shopping-cart/src/context/filtres.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+
+// Create a context for the filters
+// This context will be used to manage the filters in the application
+export const FiltresContext = createContext()
+
+// Create a provider for the filters context
+// This provider will be used to provide the filters context to the application
